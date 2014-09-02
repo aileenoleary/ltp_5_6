6ecb41ae3533d9e69f380818f518ce5e2c3be26e
Aileen O'Leary <aileenoleary@gmail.com>
2014-09-02 15:15:31 -0700

    "First Commit"

---
+++ FavNum.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favorite number?'
+number = gets.chomp
+number2 = number.to_i + 1
+puts number + ' is a nice number. ' + number2.to_s + ' is an even bigger number. It would make a better favorite number. :)'
