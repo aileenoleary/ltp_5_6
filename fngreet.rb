8fa88b3193c5b28c7f75f3a30f10a35c3a4b39d8
Aileen O'Leary <aileenoleary@gmail.com>
2014-09-02 15:12:00 -0700

    "First Commit for fngreet.rb"

---
+++ fngreet.rb
@@ -0,0 +1,7 @@
+puts 'Hi there, what\'s your first name?'
+namef = gets.chomp
+puts 'What\'s your middle name?'
+namem = gets.chomp
+puts 'What\'s your last name?'
+namel = gets.chomp
+puts 'It is a pleasure to meet you ' + namef + ' ' + namem + ' ' + namel + '!'
