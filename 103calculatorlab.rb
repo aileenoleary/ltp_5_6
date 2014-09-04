02b8923f67c91c3e4e7c4b57245b9633496db437
Aileen O'Leary <aileenoleary@gmail.com>
2014-09-04 11:40:21 -0700

    Corrected code step 1 print formula "2+2=4"

--- 103calculatorlab.rb
+++ 103calculatorlab.rb
@@ -7,11 +7,11 @@ def calculate(operation, num1, num2)
 	end
 end
 
-def operand(operation, add, subtract)
+def operand(operation)
 	if operation == "add"
-		puts "+"
+		return "+"
 	elsif operation == "subtract"
-		puts "-"
+		return "-"
 	end
 end
 
@@ -35,7 +35,6 @@ puts "Now, give me the second number"
 
 # I am having trouble making the operand change from "+" or "-".
 # This is so much easier: puts num1 + "+" + num2 + "=" + "#{answer}"
-#  The current code below gives me a wrong number of arguments error msg
+# The current code below gives me a wrong number of arguments error msg
 
-answer = calculate(operation, num1.to_i, num2.to_i)
-	puts "The answer to " + num1 + operand + num2 + "=" + "#{answer}"
+	puts "The answer to #{num1} #{operand(operation)} #{num2} = #{calculate(operation, num1.to_i, num2.to_i)}"
